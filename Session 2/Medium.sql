a94265572da39d97421f29c3c0938de6a8250ca4
anuj-016 <23BIA50011@cuchd.in>
2026-06-08 12:21:16 +0530

    Update Medium.sql

--- Session 2/Medium.sql
+++ Session 2/Medium.sql
@@ -1,14 +1,20 @@
-WITH user_purchases AS (
+WITH purchases AS (
     SELECT
         user_id,
-        MIN(created_at) AS first_purchase_date
-    FROM purchases
-    GROUP BY user_id
+        created_at,
+        LEAD(created_at) OVER (
+            PARTITION BY user_id
+            ORDER BY created_at, id
+        ) AS next_purchase_date,
+        ROW_NUMBER() OVER (
+            PARTITION BY user_id
+            ORDER BY created_at, id
+        ) AS rn
+    FROM amazon_transactions
 )
 
-SELECT DISTINCT p.user_id
-FROM purchases p
-JOIN user_purchases u
-ON p.user_id = u.user_id
-WHERE p.created_at > u.first_purchase_date
-  AND p.created_at <= u.first_purchase_date + INTERVAL '7 days';
+SELECT user_id
+FROM purchases
+WHERE rn = 1
+  AND next_purchase_date > created_at
+  AND next_purchase_date <= created_at + INTERVAL '7 days';
